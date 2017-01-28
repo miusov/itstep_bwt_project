0c7c5d4aeaaa21b820e4c58e1f5407feb3f4b46f
unknown <08792@A19_01.zp.itstep.org>
2017-01-28 10:38:31 +0200

    add migrations

---
+++ database/migrations/2017_01_28_094140_create_subscribers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSubscribersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subscribers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();         
+            $table->string('first_name', 128);
+            $table->string('last_name', 128);
+            $table->string('email', 256);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subscribers');
+    }
+}
